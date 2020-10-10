9caafe9db26cfe14e12957035e25e84c065f0ee1
Hugh2xm <632097792@qq.com>
2020-10-10 11:43:37 +0800

    'webInit-success'

---
+++ web/src/views/Main.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        Main
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Main"
+    }
+</script>
+
+<style scoped>
+
+</style>
