62ac78c84912e7a22f7f8913d0b398246e37ff63
Hugh2xm <632097792@qq.com>
2020-11-02 12:23:21 +0800

    'add-search'

--- web/src/views/list/SList.vue
+++ web/src/views/list/SList.vue
@@ -1,10 +1,32 @@
 <template>
-    <div class="pt-7">111111</div>
+    <el-container class="TList-box">
+        <el-header class="border-bottom mb-4">
+        </el-header>
+        <div class="search-title pl-4 pb-1 border-bottom mb-5" style="width: 20rem;">
+            <h3><i class="el-icon-search pr-2"></i>搜索结果 : {{this.$route.params.id}}</h3>
+        </div>
+        <el-main>
+            <el-card class="box-card mb-4">
+                <div class="text item" >
+                    <VueAudio v-for="(items,index) in searchList.data"
+                              :key="index"
+                              :url="items.url"
+                              :name="items.name"
+                              :BNumber="items.download">
+                    </VueAudio>
+                </div>
+            </el-card>
+        </el-main>
+    </el-container>
 </template>
 
 <script>
+    import VueAudio from "./components/VueAudio";
     export default {
         name: "SList",
+        components: {
+            VueAudio
+        },
         data() {
           return {
               searchList: ''
@@ -24,5 +46,9 @@
 </script>
 
 <style scoped>
-
+.TList-box {
+    width: 80%;
+    /*background-color: black;*/
+    margin: 5rem auto 0 auto;
+}
 </style>
